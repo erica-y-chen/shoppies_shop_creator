120c4d2c73e9c636e4772d35e39da6b87934e601
Day Davis Waterbury <day.waterbury@gmail.com>
2019-06-27 14:43:49 -0700

    Adding a brand page, a movable div, and a basic hard-coded overlay

--- shop_creator/imports/ui/containers/App.jsx
+++ shop_creator/imports/ui/containers/App.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import { Redirect, Router, Route, Switch } from 'react-router'
 import Header from '/imports/ui/components/Header.jsx';
-import Shop from '/imports/ui/containers/Shop.jsx';
+import Brand from '/imports/ui/containers/Brand.jsx';
 
-// App component - represents the whole app
 export default class App extends Component {
   render() {
     return (
       <Router history={this.props.history}>
         <div className="container">
           <Header />
-          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/brand" component={Brand} />
         </div>
       </Router>
     );
--- shop_creator/imports/ui/containers/Shop.jsx
+++ shop_creator/imports/ui/containers/Shop.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import Catalog from '/imports/ui/containers/Catalog.jsx';
 import ProductForm from '/imports/ui/components/ProductForm.jsx';
 
-// App component - represents the whole app
 export default class Shop extends Component {
   render() {
     return (
