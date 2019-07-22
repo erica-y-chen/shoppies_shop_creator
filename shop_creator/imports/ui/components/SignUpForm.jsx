3bb722eae529a3b5f06aa594f77049ee4ad41d63
Erica <eyufanchen@gmail.com>
2019-07-22 15:10:59 -0700

    added animations for sign up page

--- shop_creator/imports/ui/components/SignUpForm.jsx
+++ shop_creator/imports/ui/components/SignUpForm.jsx
@@ -4,24 +4,27 @@ import './css/signupform.css'
 import InputField from './InputField.jsx'
 import SocialMedia from './SocialMediaInput.jsx'
 
-//import images
-// import Twitter from '../../../public/images/twitter_logo.svg'
-// import Instagram from '../../../public/images/instagram_logo.svg'
-// import Youtube from '../../../public/images/youtube_logo.svg'
+//libraries
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default class SignUpForm extends Component {
+
     render() {
+        //initiating animate on scroll library
+        AOS.init();
+
         return (
           // Import result is the URL of your image
           <div className="form-container">
-              <div className="user-information">
+              <div className="user-information" data-aos="fade-up" data-aos-duration="1300">
                 {/* <div className="section-header">Social Media Links</div> */}
                 <InputField name="firstName" placeHolder = "First Name *" label="First Name"/> 
                 <InputField name="lastName" placeHolder = "Last Name *" label="Last Name"/> 
                 <InputField name="email" placeHolder = "Email *" label="Email"/> 
                 <InputField name="phoneNum" placeHolder = "Phone Number *" label="Phone Number"/> 
               </div>
-              <div className="user-social-media">
+              <div className="user-social-media" data-aos="fade-left" data-aos-duration="1200" data-aos-delay="300">
                 <div className="section-header">Social Media Links</div>
                 <SocialMedia placeHolder = "Twitter" img = "twitter"/> 
                 <SocialMedia placeHolder = "Instagram" img = "instagram"/> 
